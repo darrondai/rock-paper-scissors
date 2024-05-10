4302c4199d4a05abd12b85e4029e3e3c09ed74a5
Darron Dai <daidarron@gmail.com>
2024-05-10 15:15:30 -0700

    Add understanding comments to script

---
+++ rock-paper-scissors.js
@@ -0,0 +1,15 @@
+// understand
+// the goal of the project is to build a game that is played entirely in the console,
+// The players will have 3 possible choices: rock, paper, or scissors
+// Users will input their actions through text prompts and commands?
+// The computer choice will be randomly decided
+// The game will decide the winner by comparing the user choice vs computer choice
+// rock beats scissors, loses to paper
+// paper beats rock, loses to scissors
+// scissors beats paper, loses to rock
+
+// planning
+
+// pseudocode
+
+// divide and conquer
